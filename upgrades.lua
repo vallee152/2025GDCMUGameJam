1633fb601bb79a28b0de6b047b70f711a45c3116
AttemptAtCleverness <leiffitzsimmons@gmail.com>
2025-03-29 17:37:08 -0600

    Added cost, scale, max

--- upgrades.lua
+++ upgrades.lua
@@ -2,18 +2,30 @@ upgrades = {
   jumpBoost = {
     title = "Jump Servos",
     desc = "Increases jump force, letting you jump higher in strong gravity."
+    cost = 1
+    scale = 2
+    max = 0 -- no maximum
   };
   timeSlow = {
     title = "Improved Processor",
     desc = "Improves your processing speed, essentially slowing down time from your perspective. Press Q to toggle."
+    cost = 2
+    scale = 2
+    max = 0
   };
   sprint = {
     title = "Propulsion Capacitors",
     desc = "Allows you a quick burst of speed: hold Shift to accelerate. More upgrades let you stay at high speed for longer."
+    cost = 1
+    scale = 2
+    max = 0
   };
   doubleJump = {
     title = "RCS Thrusters",
     desc = "Allows you to 'jump' in mid-air. More upgrades let you jump more times before you touch the ground."
+    cost = 3
+    scale = 2
+    max = 5
   };
   
 }
