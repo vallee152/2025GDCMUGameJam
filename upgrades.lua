1ce58c551cda0ff0063c833d7cbf82ff702171ab
AttemptAtCleverness <leiffitzsimmons@gmail.com>
2025-03-29 17:19:58 -0600

    Create upgrades.lua

---
+++ upgrades.lua
@@ -0,0 +1,19 @@
+{
+  jumpBoost = {
+    title = "Jump Servos",
+    desc = "Increases jump force, letting you jump higher in strong gravity."
+  };
+  timeSlow = {
+    title = "Improved Processor",
+    desc = "Improves your processing speed, essentially slowing down time from your perspective. Press Q to toggle."
+  };
+  sprint = {
+    title = "Propulsion Capacitors",
+    desc = "Allows you a quick burst of speed: hold Shift to accelerate. More upgrades let you stay at high speed for longer."
+  };
+  doubleJump = {
+    title = "RCS Thrusters",
+    desc = "Allows you to 'jump' in mid-air. More upgrades let you jump more times before you touch the ground."
+  };
+  
+}
