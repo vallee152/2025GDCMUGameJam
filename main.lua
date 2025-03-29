3d1dc4603114be2408dab4e232ab2326d2700aca
Valley152 <53654126+Valley152@users.noreply.github.com>
2025-03-29 10:39:00 -0600

    Made game.lua, main imports HUMP and game

--- main.lua
+++ main.lua
@@ -1,25 +1,23 @@
 -- imports
+Gamestate = require "lib.hump.gamestate"
+Timer = require "lib.hump.timer"
+Vector = require "lib.hump.vector"
+Class = require "lib.hump.class"
+Signal = require "lib.hump.signal"
+Camera = require "lib.hump.camera"
+game = require "game"
+
 
 function love.load()
-    -- ran at beginning, set up variables/globals/constants
-    -- list of subcalls from the main love functions
-    gameState = {
-        [0] = MainMenu,
-        [1] = Game
-    }
-    currState = 0
+    love.graphics.setDefaultFilter('nearest', 'nearest', 0)
 end
 
 
 function love.update(dt)
-    -- game tick
-    -- call the required subfunction based on current state
-    currState = gameState[currState].update(dt)
+    
 end
 
 
 function love.draw()
-    -- render game
-    -- call the required subfunction based on current state
-    gameState[currState].draw()
+    
 end
