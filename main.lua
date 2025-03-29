31c69080cd087298c15d53748c2dc0ea9c848d43
Valley152 <53654126+Valley152@users.noreply.github.com>
2025-03-29 11:30:16 -0600

    Started compatibility with HUMP Gamestate

--- main.lua
+++ main.lua
@@ -5,19 +5,17 @@ Vector = require "lib.hump.vector"
 Class = require "lib.hump.class"
 Signal = require "lib.hump.signal"
 Camera = require "lib.hump.camera"
-game = require "game"
-
+require "game"
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest', 0)
+    Gamestate.switch(game)
 end
-
 
 function love.update(dt)
-    
+    Gamestate.update(dt)
 end
 
-
 function love.draw()
-    
+    Gamestate.draw()
 end
