6106b121c0275770bb75c4afe0e969d872502d30
Cypher0803 <Sidney.Mryglod06@gmail.com>
2025-03-30 17:20:15 -0600

    Speed of death and sprint fix

--- level3.lua
+++ level3.lua
@@ -1,16 +1,17 @@
 startingPower = true
 gravity = 600
 local startx = 18
-local starty = 324
+local starty = 30
+death = 4000
 
 platforms = {
   floor1 = Platform(350, 100, 350, 10, 0),
-  floor2 = Platform(1032, 400, 32, 10, 0),
+  floor2 = Platform(1082, 400, 32, 10, 0),
   --wall = Platform(200, 360, 32, 32, 0)
 }
 
 movingPlatforms = {
-  flyingBox = MovingPlatform(700, 100, 50, 10, 80, 80, 1000, 400, 0)
+  flyingBox = MovingPlatform(750, 100, 50, 10, 80, 80, 1000, 400, 0)
 }
 
 coins = {}
